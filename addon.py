34c36cb2456fad787cbe78cd4b2ef20e542aa53c
leopheard <leopheard.misc@gmail.com>
2019-09-03 21:03:23 -0400

    upload

--- addon.py
+++ addon.py
@@ -29,22 +29,22 @@ def main_menu():
 
 @plugin.route('/episodes1/')
 def episodes1():
-    soup = mainaddon.get_soup(URL1)
+    soup1 = mainaddon.get_soup1(url1)
     playable_podcast1 = mainaddon.get_playable_podcast1(soup1)
     items = mainaddon.compile_playable_podcast1(playable_podcast1)
     return items
 
 @plugin.route('/episodes2/')
 def episodes2():
-    soup2 = mainaddon.get_soup2(URL2)
-    playable_podcast = mainaddon.get_playable_podcast2(soup2)
+    soup2 = mainaddon.get_soup2(url2)
+    playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
     items = mainaddon.compile_playable_podcast2(playable_podcast2)
     return items
 
 @plugin.route('/episodes3/')
 def episodes3():
-    soup3 = mainaddon.get_soup3(URL3)
-    playable_podcast = mainaddon.get_playable_podcast3(soup3)
+    soup3 = mainaddon.get_soup3(url3)
+    playable_podcast3 = mainaddon.get_playable_podcast3(soup3)
     items = mainaddon.compile_playable_podcast3(playable_podcast3)
     return items
 
